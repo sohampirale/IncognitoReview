d71657b969cf5360aff0fa0428c5c34368a28ac0
rohanmane12 <rohanmane9993@gmail.com>
2025-07-11 06:27:44 +0000

    added ignore lint for deploying

--- src/utils/DBError.ts
+++ src/utils/DBError.ts
@@ -2,7 +2,7 @@ class DBError extends Error{
     statusCode:number;
     error?:unknown
 
-    constructor(statusCode:number,message:string,error?:any){
+    constructor(statusCode:number,message:string,error?:unknown){
         super("DB Error :: "+message)
         this.statusCode=statusCode;
         this.error=error;
